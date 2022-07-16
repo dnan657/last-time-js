4289c3aa3019beee5e1b7515eee662e60bbd04b2
dnan657 <96710971+dnan657@users.noreply.github.com>
2022-07-17 04:18:42 +0600

    Update last_time.js

--- last_time.js
+++ last_time.js
@@ -1,4 +1,4 @@
-function last_time(str_date){
+function f_date_last(str_date){
 	let d_now = new Date();
 	let d_old = new Date(str_date)
 	let diff_m = (d_now.getTime() - d_old.getTime()) / (60 * 1000);
@@ -6,14 +6,15 @@ function last_time(str_date){
 
 	let str_time = '';
 	if(diff_m < 60 ){ // 1-60 мин
-		str_time = Math.floor(diff_m) || 1;
-		str_time = str_time + ' мин.'
+		str_time = Math.floor(diff_m);
+		str_time = str_time < 0 ? 1 : str_time;
+		str_time = str_time + ' мин'
 	}else if(diff_m < 24*60){ // 1-24 ч
 		str_time = Math.floor(diff_m / 60);
-		str_time = str_time + ' ч.'
+		str_time = str_time + ' ч'
 	}else if(diff_m < 7*24*60 ){ // 1-7 д
 		str_time = Math.floor(diff_m / (24*60));
-		str_time = str_time + ' д.'
+		str_time = str_time + ' д'
 	}else if(d_now.getFullYear() != d_old.getFullYear() ){ // 11 июня 2022 г.
 		str_time = d_old.getDate() + ' ' + arr_month[d_old.getMonth()] + ' ' + d_old.getFullYear() + ' г.';
 	}else{ // 11 июня
